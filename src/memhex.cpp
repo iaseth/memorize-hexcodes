d37f8c9b5282736b074acbca243cb13c246d0de4
Ankur Seth <iankurseth@gmail.com>
2021-05-16 11:24:41 +0530

    now using cpp version headers

--- src/memhex.cpp
+++ src/memhex.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 int main (int argc, char const *argv[])
 {
 	//std::cout << "Memorize hex codes!\n";
 
-	srand(time(NULL));
+	std::srand(time(NULL));
 
+	auto rand = std::rand;
 	for (int i = 0; i < 10; ++i)
 	{
 		int r = rand() % 256;
 		int g = rand() % 256;
 		int b = rand() % 256;
-		printf("\tRGB (%d, %d, %d)\n", r, g, b);
+		std::printf("\tRGB (%d, %d, %d)\n", r, g, b);
 	}
 	return 0;
 }
