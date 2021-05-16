8309afe7ac954464046ac55629aab1ae466f0b25
Ankur Seth <iankurseth@gmail.com>
2021-05-16 11:09:54 +0530

    listing 10 random colors

--- src/memhex.cpp
+++ src/memhex.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main (int argc, char const *argv[])
 {
-	std::cout << "Memorize hex codes!\n";
+	//std::cout << "Memorize hex codes!\n";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; ++i)
+	{
+		int r = rand() % 256;
+		int g = rand() % 256;
+		int b = rand() % 256;
+		printf("\tRGB (%d, %d, %d)\n", r, g, b);
+	}
 	return 0;
 }
